abf2d494b65bb13411bbe34d67608133c77a163e
Ali Gamal Ali <hictors@gmail.com>
2020-10-07 20:25:55 +0200

    Finishing all the main functions of the game

--- app.js
+++ app.js
@@ -5,39 +5,121 @@ const app = Vue.createApp({
       monsterHealth: 100,
       gameEvent: [],
       isGameRunning: false,
+      winner: "",
     };
   },
-  methods: {
-    Attack(min, max) {
-      let monsterDamage = this.calculateDamage(min, max);
-      this.playerHealth -= monsterDamage;
-      if (this.checkGameOver()) return;
-      let playerDamage = this.calculateDamage(min, max);
-      this.monsterHealth -= playerDamage;
+  computed: {
+    logEventOnScreen() {
+      return [
+        { "log--player": this.gameEvent.isPlayer },
+        { "log--monster": this.gameEvent.isPlayer },
+      ];
     },
-    specialAttack(min, max) {
-      this.playerHealth -= this.calculateDamage(min, max);
+    healthBarMonster() {
+      if (this.monsterHealth <= 0) {
+        return {
+          width: 0 + "%",
+        };
+      } else {
+        return {
+          width: this.monsterHealth + "%",
+        };
+      }
+    },
+    healthBarPlayer() {
+      if (this.playerHealth <= 0) {
+        return {
+          width: 0 + "%",
+        };
+      } else {
+        return {
+          width: this.playerHealth + "%",
+        };
+      }
+    },
+  },
+  methods: {
+    giveUp() {
+      this.isGameRunning = false;
+      this.winner = "You Forfeit , Monster Won already! ";
+    },
+    Attack() {
+      this.playerAttack(5, 10);
       if (this.checkGameOver()) return;
-      this.monsterHealth -= this.calculateDamage(min, max);
+      this.monsterAttack();
+      console.log(this.gameEvent);
+      console.log(this.gameEvent.length);
+    },
+    specialAttack() {
+      this.playerAttack(10, 20);
+      if (this.checkGameOver()) return;
+      this.monsterAttack();
+      console.log(this.gameEvent);
+      console.log(this.gameEvent.length);
     },
     calculateDamage(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
+    playerAttack(min, max) {
+      this.checkGameOver();
+      let playerDamage = this.calculateDamage(min, max);
+      this.monsterHealth -= playerDamage;
+      this.gameEvent.unshift({
+        text: `the Player hits the Monster for >> `,
+        isPlayer: true,
+        damage: playerDamage,
+      });
+    },
     monsterAttack() {
-      return Math.max(Math.floor(Math.random() * 15) + 1, 12);
+      let monsterDamage = this.calculateDamage(7, 15);
+      this.playerHealth -= monsterDamage;
+      this.checkGameOver();
+      this.gameEvent.unshift({
+        text: `the monster hits the player for >> `,
+        isPlayer: false,
+        damage: monsterDamage,
+      });
+    },
+    healPlayer() {
+      if (this.playerHealth >= 90) {
+        this.monsterAttack();
+        return;
+      } else {
+        this.playerHealth += 10;
+        this.monsterAttack();
+      }
+      this.gameEvent.unshift({
+        text: `player heals for 10 hp`,
+        isHeal: true,
+      });
     },
     checkGameOver() {
       if (this.playerHealth <= 0) {
-        console.log("monster Wins");
-        this.playerHealth = 100;
-        this.monsterHealth = 100;
+        if (confirm("Monster Won ! , Start new Game?")) {
+          console.log("monster Wins");
+          this.startNewGame();
+        } else {
+          this.isGameRunning = false;
+          this.winner = "Monster Won !, You will get him Next time";
+        }
         return true;
       } else if (this.monsterHealth <= 0) {
-        console.log("player wins ");
-        this.playerHealth = 100;
-        this.monsterHealth = 100;
+        if (confirm("Player Won ! , Start new Game?")) {
+          console.log("monster Wins");
+          this.startNewGame();
+        } else {
+          this.isGameRunning = false;
+          this.winner = "You Destroyed the Monster,Well Done!";
+        }
         return true;
       }
+      return false;
+    },
+    startNewGame() {
+      this.isGameRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.gameEvent = [];
     },
   },
 });
